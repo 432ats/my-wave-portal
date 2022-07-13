cbca10e7737e2b72c6c7340b31e1b1e6b05a40e0
shimizu-atsushi <ats.8060@gmail.com>
2022-07-14 02:33:48 +0900

    modified run.js

--- scripts/run.js
+++ scripts/run.js
@@ -5,51 +5,78 @@
 */
 const main = async () => {
 
-  // hre.ethers.getSigners() は Hardhat が提供する任意のアドレスを返す関数
-  const [owner, randomPerson1, randomPerson2] = await hre.ethers.getSigners();
-  /*
-    WavePortal コントラクトをコンパイル
-    コントラクトがコンパイルされたら、コントラクトを扱うために必要なファイルが artifacts ディレクトリの直下に生成される
-    hre.ethers.getContractFactory について getContractFactory 関数は、
-    デプロイをサポートするライブラリのアドレスと WavePortal コントラクトの連携を行う
-   */
-  const waveContactFactory = await hre.ethers.getContractFactory("WavePortal");
 
-  /*
-    Hardhat がローカルの Ethereum ネットワークを、コントラクトのためだけに作成します。
-    スクリプトの実行が完了した後、そのローカル・ネットワークを破棄します。
-    コントラクトを実行するたびに、毎回ローカルサーバを更新するかのようにブロックチェーンが新しくなります.
-    常にゼロリセットとなるので、エラーのデバッグがしやすくなります。
-  */ 
-  const waveContract = await waveContactFactory.deploy();
+  // const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  // const waveContract = await waveContractFactory.deploy();
+  // console.log("Contract added to:", waveContract.address);
+  // let waveCount;
+  // waveCount = await waveContract.getTotalWaves();
+  // console.log(waveCount.toNumber());
+  // /**
+  //  * 「👋（wave）」を送る
+  //  */
+  // let waveTxn = await waveContract.wave("A message!");
+  // await waveTxn.wait(); // トランザクションが承認されるのを待つ（テスト:1回目）
+  // const [_, randomPerson] = await hre.ethers.getSigners();
+  // waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+  // await waveTxn.wait(); // トランザクションが承認されるのを待つ（テスト:2回目）
+  // let allWaves = await waveContract.getAllWaves();
+  // console.log(allWaves);
 
-  /*
-    WavePortal コントラクトが、ローカルのブロックチェーンにデプロイされるまで待つ処理を行っています。
-    Hardhat は実際にあなたのマシン上に「マイナー」を作成し、ブロックチェーンを構築してくれます。
-    constructor は、コントラクトがデプロイされるときに初めて実行されます。
-  */
-  const wavePortal = await waveContract.deployed();
+  // // hre.ethers.getSigners() は Hardhat が提供する任意のアドレスを返す関数
+  // const [owner, randomPerson1, randomPerson2] = await hre.ethers.getSigners();
+  // /*
+  //   WavePortal コントラクトをコンパイル
+  //   コントラクトがコンパイルされたら、コントラクトを扱うために必要なファイルが artifacts ディレクトリの直下に生成される
+  //   hre.ethers.getContractFactory について getContractFactory 関数は、
+  //   デプロイをサポートするライブラリのアドレスと WavePortal コントラクトの連携を行う
+  //  */
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 
-  // wavePortal.address はデプロイされたコントラクトのアドレスを出力
-  console.log("Contract deployed to:", wavePortal.address);
-  // owner.address = deployした人のaddress(今回は自分)
-  console.log("Contract deployed by:", owner.address);
+  // /*
+  //   Hardhat がローカルの Ethereum ネットワークを、コントラクトのためだけに作成します。
+  //   スクリプトの実行が完了した後、そのローカル・ネットワークを破棄します。
+  //   コントラクトを実行するたびに、毎回ローカルサーバを更新するかのようにブロックチェーンが新しくなります.
+  //   常にゼロリセットとなるので、エラーのデバッグがしやすくなります。
+  // */ 
+  const waveContract = await waveContractFactory.deploy();
+  console.log("Contract added to:", waveContract.address);
+
+
+  // /*
+  //   WavePortal コントラクトが、ローカルのブロックチェーンにデプロイされるまで待つ処理を行っています。
+  //   Hardhat は実際にあなたのマシン上に「マイナー」を作成し、ブロックチェーンを構築してくれます。
+  //   constructor は、コントラクトがデプロイされるときに初めて実行されます。
+  // */
+  // const wavePortal = await waveContract.deployed();
+
+  // // wavePortal.address はデプロイされたコントラクトのアドレスを出力
+  // console.log("Contract deployed to:", wavePortal.address);
+  // // owner.address = deployした人のaddress(今回は自分)
+  // console.log("Contract deployed by:", owner.address);
 
   
   let waveCount;
-  // getTotalWaves() を呼び出し、既存の「👋（wave）」の総数を取得
+  // // getTotalWaves() を呼び出し、既存の「👋（wave）」の総数を取得
   waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-  // ユーザーが新しい「👋（wave）」を送ったことを承認するまで、コントラクトからの応答をフロントエンドが待機するよう設定
-  let waveTxn = await waveContract.wave();
+
+  // // ユーザーが新しい「👋（wave）」を送ったことを承認するまで、コントラクトからの応答をフロントエンドが待機するよう設定
+  let waveTxn = await waveContract.wave("A message!");
   await waveTxn.wait();
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
 
-  waveCount = await waveContract.getTotalWaves();
 
-  waveTxn = await waveContract.connect(randomPerson1).wave();
+  // waveCount = await waveContract.getTotalWaves();
+
+  // waveTxn = await waveContract.connect(randomPerson1).wave();
   await waveTxn.wait();
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 
-  waveCount = await waveContract.getTotalWaves();
+  // waveCount = await waveContract.getTotalWaves();
 
 };
 
